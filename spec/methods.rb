9bc07623116d72b5c56c1f1cd1e6e0d283755b74
Mbogz <jeff.mwaura@student.moringaschool.com>
2023-06-20 17:20:47 +0300

    Had some trouble

---
+++ spec/methods.rb
@@ -0,0 +1,50 @@
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+
+greet_programmer
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+
+end
+
+greet "Naureen"
+
+
+
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+
+end
+greet_with_default
+greet_with_default "Sunny"
+
+
+
+
+
+
+def add_and_return(num1, num2)
+   return num1 + num2
+end
+sum = add_and_return(1, 2)
+
+
+
+
+
+
+
+def halve(numer)
+    if name.class != number
+        return null
+    end
+
+    numer.halve
+end
+halve
